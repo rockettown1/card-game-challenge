2141379750d794459559c4ef4ac44e919a19b8a7
Dan Krishnan <dan@wearecodenation.com>
2020-02-16 22:32:44 +0000

    inital state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import "./App.css";
+import Bowser from "./images/bowser.jpg";
+import BabyMario from "./images/babymario.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    message: "match the cards to win the game",
+    cards: [
+      { flipped: false, image: Bowser },
+      { flipped: false, image: BabyMario },
+      { flipped: false, image: Bowser },
+      { flipped: false, image: BabyMario }
+    ],
+    firstFlip: null,
+    secondFlip: null
+  };
+
+  flipHandler = index => {
+    if (this.state.firstFlip == null) {
+      let newCards = this.state.cards;
+      newCards[index].flipped = true;
+      this.setState({ cards: newCards, firstFlip: index });
+    } else if (this.state.secondFlip == null) {
+      let newCards = this.state.cards;
+      newCards[index].flipped = true;
+      this.setState({ cards: newCards, secondFlip: index });
+    }
+  };
+
+  //this is a React Lifecycle method - read the docs
+  componentDidUpdate() {
+    //object destructuring so I don't have to keep typing this.state
+    const { firstFlip, secondFlip, cards } = this.state;
+    if (firstFlip != null && secondFlip != null) {
+      if (cards[firstFlip].image == cards[secondFlip].image) {
+        console.log("its a match");
+        this.setState({ firstFlip: null, secondFlip: null });
+      } else if (cards[firstFlip].image != cards[secondFlip].image) {
+        setTimeout(() => {
+          let newCards = this.state.cards;
+          newCards[firstFlip].flipped = false;
+          newCards[secondFlip].flipped = false;
+          this.setState({ cards: newCards, firstFlip: null, secondFlip: null });
+        }, 1000);
+      }
+    }
+    this.winningLogic();
+  }
+
+  winningLogic = () => {
+    if (
+      this.state.cards.every(card => {
+        return card.flipped == true;
+      })
+    ) {
+      console.log("a winner");
+    }
+  };
+
+  render() {
+    return (
+      <div className="board">
+        {this.state.cards.map((card, index) => {
+          return <Card key={index} image={card.image} flipped={card.flipped} click={() => this.flipHandler(index)} />;
+        })}
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
